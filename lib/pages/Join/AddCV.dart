a08654b85a517381a33314a9310e0c2ca9a9ae49
usef-sudo <yousefalnajjar444@gmail.com>
2021-04-17 17:56:35 +0300

    mohanad

--- lib/pages/Join/AddCV.dart
+++ lib/pages/Join/AddCV.dart
@@ -4,6 +4,7 @@ import 'package:path/path.dart' as p;
 import 'package:flutter/material.dart';
 import 'package:hypercv/Helpers/Device.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:video_player/video_player.dart';
 
 class AddCV extends StatefulWidget {
   @override
@@ -11,9 +12,10 @@ class AddCV extends StatefulWidget {
 }
 
 class _AddCVState extends State<AddCV> {
+  VideoPlayerController _controller;
 
-  List<File>fileList = new List<File>();
-File _certification;
+  List<File> fileList = new List<File>();
+  File _certification;
 
   @override
   Widget build(BuildContext context) {
@@ -32,21 +34,22 @@ File _certification;
                   decoration: BoxDecoration(
                     border: Border.all(width: 1.0),
                     borderRadius: BorderRadius.all(Radius.circular(
-                        10.0) //                 <--- border radius here
-                    ),
+                            10.0) //                 <--- border radius here
+                        ),
                   ),
                   child: Icon(Icons.arrow_back)),
             ),
           ),
           centerTitle: true,
-          actions: [Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text("SKIP",style:
-            TextStyle(color: Colors.black)),
-          )],
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("SKIP", style: TextStyle(color: Colors.black)),
+            )
+          ],
           title: Text('',
               style:
-              TextStyle(fontWeight: FontWeight.bold, color: Colors.black)),
+                  TextStyle(fontWeight: FontWeight.bold, color: Colors.black)),
           elevation: 0,
           iconTheme: IconThemeData(color: Colors.black),
           backgroundColor: Colors.white),
@@ -58,7 +61,8 @@ File _certification;
               child: Text(
                 "ADD Video CV ",
                 style: TextStyle(
-                    fontWeight: FontWeight.bold, fontSize: Device.height * 0.03),
+                    fontWeight: FontWeight.bold,
+                    fontSize: Device.height * 0.03),
               ),
             ),
           ),
@@ -75,58 +79,123 @@ File _certification;
                 child: Container(
                     width: Device.width * 0.6,
                     height: Device.height * 0.1,
-                    child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,children: [
-                      (_certification!=null) ?
-                      Stack(
-                        children: [
-                          Container(
-                              child: Column(
-                                  crossAxisAlignment:
-                                  CrossAxisAlignment
-                                      .center,
-                                  mainAxisAlignment:
-                                  MainAxisAlignment
-                                      .center,
-                                  children: <Widget>[
-                                    Icon(Icons
-                                        .insert_drive_file),
-                                    Text(p.extension(
-                                        _certification.path))
-                                  ])),
-
-                        ],
-                      ):Container(child: Text("Attach MP4"),),
-                      (_certification!=null) ? IconButton(icon: Icon(Icons.clear), onPressed: (){setState(() {
-                        _certification=null;
-                      });}): IconButton(icon: Icon(Icons.attach_file), onPressed: (){addfile();})
-
-                    ],)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        (_certification != null)
+                            ? Stack(
+                                children: [
+                                  Container(
+                                      child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.center,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: <Widget>[
+                                        Icon(Icons.insert_drive_file),
+                                        Text(p.extension(_certification.path))
+                                      ])),
+                                ],
+                              )
+                            : Container(
+                                child: Text("Attach MP4"),
+                              ),
+                        (_certification != null)
+                            ? IconButton(
+                                icon: Icon(Icons.clear),
+                                onPressed: () {
+                                  setState(() {
+                                    _certification = null;
+                                  });
+                                })
+                            : IconButton(
+                                icon: Icon(Icons.attach_file),
+                                onPressed: () {
+                                  addfile().then((value) {
+                                    _controller = VideoPlayerController.file(
+                                        _certification);
+                                    _controller.addListener(() {
+                                      setState(() {});
+                                    });
+                                    _controller.setLooping(true);
+                                    _controller.initialize().then((_) => setState(() {}));
+                                    _controller.play();
+                                  });
+                                })
+                      ],
+                    )),
               ),
             ),
           ),
           Container(
             height: Device.height * 0.02,
           ),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Describe your video",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: Device.height * 0.02),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: () {
+                  if (_certification == null) {
+                  } else {
+                    showDialog(
+                        context: context,
+                        builder: (_) => new AlertDialog(
+                              title: new Container(
+                                height: 200,
+                                width: 200,
+                                child: AspectRatio(
 
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [
-          Padding(
-          padding: const EdgeInsets.all(8.0),
-    child: Text(
-    "Describe your video",
-    style: TextStyle(
-    fontWeight: FontWeight.bold, fontSize: Device.height * 0.02),
-    ),
-    ),
-    Padding(
-    padding: const EdgeInsets.all(8.0),
-    child: Text(
-    "Preview",//todo make it show the _certification video on alert
-    style: TextStyle(
-    fontWeight: FontWeight.bold, fontSize: Device.height * 0.02),
-    ),
-    ),
-]
-          ),
+                                  aspectRatio: _controller.value.aspectRatio,
+                                  // Use the VideoPlayer widget to display the video.
+                                  child: VideoPlayer(_controller),
+                                ),
+                              ),
+                              actions: <Widget>[
+                                FlatButton(
+                                  child: Text('Back'),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                                FlatButton(
+                                  child: Text('Play/Pause'),
+                                  onPressed: () {
+                                    print(_controller.dataSource);
+                                    // Wrap the play or pause in a call to `setState`. This ensures the
+                                    // correct icon is shown
+                                    setState(() {
+                                      // If the video is playing, pause it.
+                                      if (_controller.value.isPlaying) {
+                                        _controller.pause();
+                                      } else {
+                                        // If the video is paused, play it.
+                                        _controller.play();
+                                      }
+                                    });
+                                  },
+                                ),
+                              ],
+                            ));
+                  }
+                },
+                child: Text(
+                  "Preview", //todo make it show the _certification video on alert
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: Device.height * 0.02),
+                ),
+              ),
+            ),
+          ]),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Card(
@@ -138,10 +207,9 @@ File _certification;
                 child: Padding(
                   padding: EdgeInsets.all(8.0),
                   child: TextFormField(
-                   // onChanged: (val) => _description = val,
+                    // onChanged: (val) => _description = val,
                     maxLines: 5,
-                    decoration: InputDecoration.collapsed(
-                       ),
+                    decoration: InputDecoration.collapsed(),
                   ),
                 )),
           ),
@@ -154,55 +222,39 @@ File _certification;
               child: Text(
                 "Add Reference Images",
                 style: TextStyle(
-                    fontWeight: FontWeight.bold, fontSize: Device.height * 0.03),
+                    fontWeight: FontWeight.bold,
+                    fontSize: Device.height * 0.03),
               ),
             ),
           ),
           Container(
             child: Padding(
-              padding:
-              EdgeInsets.all(Device.height * 0.006),
+              padding: EdgeInsets.all(Device.height * 0.006),
               child: GridView.builder(
-                  itemCount:
-                  fileList.length + 1,
+                  itemCount: fileList.length + 1,
                   shrinkWrap: true,
-                  gridDelegate:
-                  SliverGridDelegateWithFixedCrossAxisCount(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 3,
-                      crossAxisSpacing:
-                      Device.height * 0.005,
-                      mainAxisSpacing:
-                      Device.height * 0.005,
+                      crossAxisSpacing: Device.height * 0.005,
+                      mainAxisSpacing: Device.height * 0.005,
                       childAspectRatio:
-                      ((MediaQuery.of(context)
-                          .size
-                          .width -
-                          30) /
-                          2) /
-                          240), //width/height
+                          ((MediaQuery.of(context).size.width - 30) / 2) /
+                              240), //width/height
                   physics: ScrollPhysics(),
-                  itemBuilder:
-                      (BuildContext context, int i) {
-                    List<String> picExt = [
-                      '.jpg',
-                      '.jpeg',
-                      '.bmp',
-                      '.png'
-                    ];
+                  itemBuilder: (BuildContext context, int i) {
+                    List<String> picExt = ['.jpg', '.jpeg', '.bmp', '.png'];
                     if (i == 0) {
                       return InkWell(
                         onTap: pickFiles,
                         child: Container(
                             child: Icon(
-                              Icons.add,
-                              size: Device.height * 0.05,
-                            )),
+                          Icons.add,
+                          size: Device.height * 0.05,
+                        )),
                       );
                     } else {
-                      if (fileList != null &&
-                         fileList.length >
-                              0) if (picExt.contains(
-                          p.extension(fileList[i - 1].path))) {
+                      if (fileList != null && fileList.length > 0) if (picExt
+                          .contains(p.extension(fileList[i - 1].path))) {
                         return InkWell(
                           onTap: () {
                             removeA(i - 1);
@@ -215,10 +267,8 @@ File _certification;
                                 right: 0.0,
                                 child: IconButton(
                                   icon: Icon(
-                                    Icons
-                                        .remove_circle_outline,
-                                    color:
-                                    Colors.red[800],
+                                    Icons.remove_circle_outline,
+                                    color: Colors.red[800],
                                   ),
 // onPressed:removeA(i - 1)
                                 ),
@@ -236,28 +286,20 @@ File _certification;
                               Container(
                                   child: Column(
                                       crossAxisAlignment:
-                                      CrossAxisAlignment
-                                          .center,
+                                          CrossAxisAlignment.center,
                                       mainAxisAlignment:
-                                      MainAxisAlignment
-                                          .center,
+                                          MainAxisAlignment.center,
                                       children: <Widget>[
-                                        Icon(Icons
-                                            .insert_drive_file),
-                                        Text(p.extension(
-                                            fileList[
-                                            i - 1]
-                                                .path))
-                                      ])),
+                                    Icon(Icons.insert_drive_file),
+                                    Text(p.extension(fileList[i - 1].path))
+                                  ])),
                               Positioned(
                                 top: 0.0,
                                 right: 0.0,
                                 child: IconButton(
                                   icon: Icon(
-                                    Icons
-                                        .remove_circle_outline,
-                                    color:
-                                    Colors.red[800],
+                                    Icons.remove_circle_outline,
+                                    color: Colors.red[800],
                                   ),
 // onPressed:removeA(i - 1)
                                 ),
@@ -272,11 +314,11 @@ File _certification;
 // color: Colors.red,
 // height: 200,
 // width: 200,
-                    );
+                        );
                   }
 
 //fileListThumb,
-              ),
+                  ),
             ),
             height: Device.height * 0.35,
             color: Color.fromRGBO(238, 238, 255, 1),
@@ -303,18 +345,15 @@ File _certification;
                 child: Padding(
                   padding: EdgeInsets.all(8.0),
                   child: TextFormField(
-                   // onChanged: (val) => _description = val,
+                    // onChanged: (val) => _description = val,
                     maxLines: 5,
-                    decoration: InputDecoration.collapsed(
-                       ),
+                    decoration: InputDecoration.collapsed(),
                   ),
                 )),
           ),
-
           Container(
             height: Device.height * 0.02,
           ),
-
           InkWell(
             onTap: () {
 //todo must create user using ali api
@@ -334,16 +373,16 @@ File _certification;
                   child: Container(
                       decoration: BoxDecoration(
                           border:
-                          Border.all(color: Colors.green[800], width: 1.0),
+                              Border.all(color: Colors.green[800], width: 1.0),
                           borderRadius:
-                          BorderRadius.all(Radius.circular(20.0) //
-                          )),
+                              BorderRadius.all(Radius.circular(20.0) //
+                                  )),
                       child: Center(
                           child: Text(
-                            "Add Vedieo CV",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, color: Colors.white),
-                          ))),
+                        "Add Vedieo CV",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.white),
+                      ))),
                 ),
               ),
             ),
@@ -362,7 +401,6 @@ File _certification;
         'jpeg',
         'bmp',
         'pdf',
-
       ],
     ).then((files) {
       setState(() {
@@ -373,7 +411,6 @@ File _certification;
       });
     });
   }
-
 
   removeA(int index) {
     //print(index.toString() + "from tabbed");
@@ -388,7 +425,7 @@ File _certification;
             // usually buttons at the bottom of the dialog
             FlatButton(
               child: new Text(
-               "Close",
+                "Close",
                 style: TextStyle(color: Colors.blue),
               ),
               onPressed: () {
@@ -416,10 +453,10 @@ File _certification;
   Future addfile() async {
     await FilePicker.getFile(
       type: FileType.video,
-
     ).then((file) {
       setState(() {
-        _certification =file;
+        _certification = file;
       });
     });
-  }}
+  }
+}
